c8db20516b402f3b1980e79a56992dcff387dd92
Gease-RS <geasebrasil@gmail.com>
2022-01-15 22:05:53 -0300

    Component Car

--- src/app/components/button/index.tsx
+++ src/app/components/button/index.tsx
@@ -4,6 +4,7 @@ import tw from "twin.macro";
 interface IButtonProps {
   theme?: "filled" | "outlined";
   text: string;
+  className?: string;
 }
 
 const BaseButton = styled.button`
@@ -49,8 +50,9 @@ const FilledButton = styled(BaseButton)`
 `;
 
 export function Button(props: IButtonProps) {
-  const { theme, text } = props;
+  const { theme, text, className } = props;
 
-  if (theme === "filled") return <FilledButton>{text}</FilledButton>;
-  else return <OutlineButton>{text}</OutlineButton>;
+  if (theme === "filled")
+    return <FilledButton className={className}>{text}</FilledButton>;
+  else return <OutlineButton className={className}>{text}</OutlineButton>;
 }
