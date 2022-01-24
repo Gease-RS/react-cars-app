148d8d306ab38d9c49d2acd2244b0d995f3bc401
Gease-RS <geasebrasil@gmail.com>
2022-01-23 23:02:02 -0300

    SectionContainer

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -8,7 +8,8 @@ const FooterContainer = styled.footer`
   ${tw`
     flex
     min-w-full
-    pt-7
+    pt-4
+    md:pt-12
     pb-1
     items-center
     justify-center
@@ -18,7 +19,6 @@ const FooterContainer = styled.footer`
 const InnerContainer = styled.div`
   ${tw`
     flex
-    flex-col
     w-full
     h-full
     max-w-screen-2xl
@@ -27,10 +27,62 @@ const InnerContainer = styled.div`
 
 const Aboutcontainer = styled.div`
   ${tw`
-        flex
-        flex-col
+    flex
+    flex-col
+    mr-2
+    md:mr-12
+  `}
+`;
 
+const AboutText = styled.p`
+  ${tw`
+    text-white
+    text-sm
+    font-normal
+    max-w-xs
+    leading-5
+    mt-2
+  `}
+`;
+
+const SectionContainer = styled.div`
+  ${tw`
+    flex
+    flex-col
+    mr-2
+    md:mr-12
+
+  `};
+`;
+
+const LinksList = styled.ul`
+  ${tw`
+    outline-none
+    list-none
+    flex
+    flex-col
+  `};
+`;
+
+const ListItem = styled.li`
+  ${tw`
+    mb-3
+  `}
+  & > a {
+    ${tw`
+      text-sm
+      text-white
     `}
+  }
+`;
+
+const HeaderTitle = styled.h3`
+  ${tw`
+    text-2xl
+    font-bold
+    text-white
+    mb-3
+  `}
 `;
 
 export function Footer() {
@@ -39,7 +91,51 @@ export function Footer() {
       <InnerContainer>
         <Aboutcontainer>
           <Logo color="white" bgColor="dark" />
+          <AboutText>
+            Youcar is Car renting and selling company located in many countries
+            across the world which has high quality cars and top rated service.
+          </AboutText>
         </Aboutcontainer>
+        <SectionContainer>
+          <HeaderTitle>About</HeaderTitle>
+          <LinksList>
+            <ListItem>
+              <a href="#">Home</a>
+            </ListItem>
+            <ListItem>
+              <a href="#">About US</a>
+            </ListItem>
+            <ListItem>
+              <a href="#">Services</a>
+            </ListItem>
+            <ListItem>
+              <a href="#">Models</a>
+            </ListItem>
+            <ListItem>
+              <a href="#">Blog</a>
+            </ListItem>
+          </LinksList>
+        </SectionContainer>
+        <SectionContainer>
+          <HeaderTitle>Other Links</HeaderTitle>
+          <LinksList>
+            <ListItem>
+              <a href="#">FAQ</a>
+            </ListItem>
+            <ListItem>
+              <a href="#">Contac Us</a>
+            </ListItem>
+            <ListItem>
+              <a href="#">Support</a>
+            </ListItem>
+            <ListItem>
+              <a href="#">Privacy Policy</a>
+            </ListItem>
+            <ListItem>
+              <a href="#">Terms & Contitions</a>
+            </ListItem>
+          </LinksList>
+        </SectionContainer>
       </InnerContainer>
     </FooterContainer>
   );
